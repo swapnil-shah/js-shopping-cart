cc711f598ffcf17c9efbf9d28e2217495cd22065
swapnil-shah <swapnilshah.me@gmail.com>
2020-12-23 19:00:39 -0500

    added code for cart badge and total price

--- shopping-cart.js
+++ shopping-cart.js
@@ -1,10 +1,13 @@
 
 import items from './items.json'
 import formatCurrency from './util/formatCurrency.js'
+
 const cartButton = document.querySelector('[data-cart-button]')
 const cartItemsWrapper = document.querySelector('[data-cart-items-wrapper]')
 const cartItemsContainer = document.querySelector('[data-cart-items]')
 const cartItemTemplate = document.querySelector('#cart-item-template')
+const cartQuantity = document.querySelector('[data-cart-quantity]')
+const cartTotal = document.querySelector('[data-cart-total]')
 const IMAGE_URL = "https://dummyimage.com/210x130"
 let shoppingCart = []
 export function setUpShoppingCart() { }
@@ -17,11 +20,31 @@ export function setUpShoppingCart() { }
 cartButton.addEventListener('click', () => {
   cartItemsWrapper.classList.toggle('invisible')
 })
+
+// Add items to Cart
 export function addToCart(id) {
-  shoppingCart.push({ id: id, quantity: 1 })
+  // Handle multiple of the same item in the cart
+  const existingItem = shoppingCart.find(entry => entry.id === id)
+  if (existingItem) {
+    existingItem.quantity++
+  }
+  else {
+    shoppingCart.push({ id: id, quantity: 1 })
+  }
   renderCart(shoppingCart)
 }
 function renderCart(shoppingCart) {
+
+  cartQuantity.innerHTML = shoppingCart.length //Cart Badge number
+
+  //Calculate Total in the cart
+  const totalCents = shoppingCart.reduce((sum, entry) => {
+    const item = items.find(i => entry.id === i.id)
+    return sum + item.priceCents * entry.quantity
+  }, 0)
+  cartTotal.innerHTML = formatCurrency(totalCents / 100)
+
+
   cartItemsContainer.innerHTML = ""
   shoppingCart.forEach(entry => {
     const item = items.find(i => entry.id === i.id)
@@ -35,10 +58,11 @@ function renderCart(shoppingCart) {
 
     const image = cartItem.querySelector('[data-image]')
     image.src = `${IMAGE_URL}/${item.imageColor}/${item.imageColor}`
-    console.log(cartItem.querySelector('[data-quantity]'))
 
-    const quantity = cartItem.querySelector('[data-quantity]')
-    quantity.innerText = `x${entry.quantity}`
+    if (entry.quantity > 1) {
+      const quantity = cartItem.querySelector('[data-quantity]')
+      quantity.innerText = `x${entry.quantity}`
+    }
 
     const price = cartItem.querySelector('[data-price]')
     price.innerText = formatCurrency(item.priceCents * entry.quantity / 100)
@@ -46,7 +70,7 @@ function renderCart(shoppingCart) {
     cartItemsContainer.appendChild(cartItem)
   })
 }
-// Add items to Cart
-  // Handle click event for adding
-  // Handle multiple of the same item in the cart
+
+
+
   // Calculate accurate total
